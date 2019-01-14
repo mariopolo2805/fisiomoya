233bdcd0e907ef2dd03d176f7c85152ea99618b0
mariopolo2805 <mariopolo2805@gmail.com>
2019-01-14 01:16:16 +0100

    Auto deploy

--- js/materialize-init.js
+++ js/materialize-init.js
@@ -2,6 +2,10 @@ $(document).ready(function() {
   $('.slider').slider({
     interval: 10000
   });
+  $('.slider').slider('next');
+  setTimeout(function() {
+    $('.slider').slider('prev');
+  }, 100);
   $('.parallax').parallax();
   $('select').formSelect();
   $('.sidenav').sidenav();
