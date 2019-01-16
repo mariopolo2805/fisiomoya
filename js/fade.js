48f2af28badea4ad91e486e192f6a6794a7d8131
mariopolo2805 <mariopolo2805@gmail.com>
2019-01-16 01:25:35 +0100

    Auto deploy

---
+++ js/fade.js
@@ -0,0 +1,28 @@
+$(window).on("load",function() {
+  $(window).scroll(function() {
+    var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+    $(".fade").each(function() {
+      var objectBottom = $(this).offset().top + $(this).outerHeight();
+
+      if (objectBottom < (windowBottom)) {
+        if ($(this).css("opacity")==0) {
+          $(this).fadeTo(500,1);
+          if ($(this).hasClass("reverse")) {
+            $(this).css("transform", "translateX(50px)");
+          } else {
+            $(this).css("transform", "translateX(-50px)");
+          }
+        }
+      } else {
+        if ($(this).css("opacity")==1) {
+          $(this).fadeTo(500,0);
+          if ($(this).hasClass("reverse")) {
+            $(this).css("transform", "translateX(-50px)");
+          } else {
+            $(this).css("transform", "translateX(50px)");
+          }
+        }
+      }
+    });
+  }).scroll();
+});
--- js/materialize-init.js
+++ js/materialize-init.js
@@ -1,13 +1,5 @@
 $(document).ready(function() {
-  $('.slider').slider({
-    interval: 10000
-  });
-  $('.slider').slider('next');
-  setTimeout(function() {
-    $('.slider').slider('prev');
-  }, 100);
   $('.parallax').parallax();
-  $('select').formSelect();
   $('.sidenav').sidenav();
   $('.modal').modal({
     inDuration: 300, // Transition in duration
