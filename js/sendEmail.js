234d0119b10c9ad38bf1fced689fce067f6a7c39
mariopolo2805 <mariopolo2805@gmail.com>
2019-01-28 19:08:14 +0100

    Auto deploy

--- js/sendEmail.js
+++ js/sendEmail.js
@@ -1,40 +1,11 @@
-(document).ready(function () {
-  $("#send").click(function () {
-      //first email list
-      var name = $("#name").val();
-      var email = $("#email").val();
-      var message = $("#message").val();
-      var isValid = true;
+$(document).ready(function () {
+  $("#contact-form").submit(function (event) {
+    var name = encodeURIComponent($("input[name='name']").val());
+    var email = encodeURIComponent($("input[name='email']").val());
+    var message = encodeURIComponent($("textarea[name='message']").val());
 
-      //validate the first email address
-      if (emailAddress1 == "") {
-          $("#email_address1_error").text("this address is required");
-          isValid = false;
-      } else {
-          $("#email_address1").next().text("");
-      }
-      //validate the second email address
-      if (emailAddress2 == "") {
-          $("#email_address2_error").text("this address is required");
-          isValid = false;
-      } else if (emailAddress1 !== emailAddress2) {
-          $("#email_address2_error").text("the two emails have to be the same");
-          isValid = false;
-      } else {
-          $("#email_address2_error").text("");
-      }
-      //validate the first Name
-      if (firstName == "") {
-          $("#first_name_error").text("name is required");
-          isValid = false;
-      } else {
-          $("#first_name_error").text("");
-      }
-      //submit the form
-      if (isValid) {
-          $("#email_form").submit();
-      }
+    $(location).attr('href', 'mailto:info@fisioeffect.es?subject=Info&body=' + message + '%0D%0A%0D%0A' + name + '%0D%0A' + email);
+
+    event.preventDefault();
   });
-  //focus the cursor on the first email address
-  $("#email_address1").focus();
 });
